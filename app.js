9efc96f0e06e8be1504c299d7fb441bb3d58008b
Nadin <nadyawinters@gmail.com>
2021-05-14 21:22:55 +0200

    testing

--- app.js
+++ app.js
@@ -44,8 +44,14 @@ app.use(errorController.respondNoResourceFound);
 
 //databse
 
-//const DB_URI = process.env.MONGODB_URI || ((process.env.NODE_ENV === 'test') ? 'mongodb://localhost:27017/modulehandbook_test_db' : 'mongodb://localhost:27017/modulehandbook_db')
-const DB_URI = process.env.MONGODB_URI || "mongodb://mongo:27017/quiz-data"; //Main DB
+const DB_URI =
+  process.env.MONGODB_URI ||
+  (process.env.NODE_ENV === "test"
+    ? "mongodb://mongo:27017/quiz_test_db"
+    : "mongodb://mongo:27017/quiz_db");
+
+//const DB_URI = process.env.MONGODB_URI || "mongodb://mongo:27017/quiz-data"; //Main DB
+
 mongoose.connect(DB_URI, { useNewUrlParser: true }).then(() => {
   app.listen(port, () => console.info(`server listening on port ${port}`));
   console.log("Main DB connected");
