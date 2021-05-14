bf7608dac6abfe8514b904c525cf013f07d20bb7
Nadin <nadyawinters@gmail.com>
2021-05-14 22:02:01 +0200

    working??

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,20 +1,23 @@
-const User = require('../models/userModel');
-const dataCTRL = require('./databaseController');
-
+const User = require("../models/userModel");
+const dataCTRL = require("./databaseController");
 
 exports.getSignupPage = (req, res) => {
-    res.render('signup');
+  res.render("signup");
 };
 
 exports.postSignup = (req, res) => {
-    dataCTRL.isertUserIntoRegistered(req);
-    console.log(req.body.username);
-    res.status(200).render('ty', {name: req.body.username});
+  dataCTRL.isertUserIntoRegistered(req);
+  console.log(req.body.username);
+  res.status(200).render("ty", { name: req.body.username });
 };
 
-exports.getAllRegisteredPage = (req, res) =>{
-    User.find({}, (err, users) => {
-        if(err) throw err;
-        res.status(200).render('allusers', {all: users});
-    })
-}
+exports.getAllRegisteredPage = (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) throw err;
+    res.status(200).render("allusers", { all: users });
+  });
+};
+
+exports.deleteAllRegistered = () => {
+  User.remove({});
+};
---
+++ test/index.test.js
@@ -0,0 +1,52 @@
+//process.env.NODE_ENV = "test";
+const mongoose = require("mongoose");
+const User = require("../models/userModel");
+const db = mongoose.connection;
+const regController = require("../controllers/RegisterController");
+
+describe("insert", () => {
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+  it("should insert a doc into collection", async () => {
+    const mockUser = {
+      name: "Dummy",
+      email: "dummy@test.de",
+      password: "password",
+      date: "01/01/2000",
+      id: 1,
+    };
+    await User.create(mockUser, (err) => {
+      if (err) throw err;
+    });
+
+    await db.collection("registered").findOne({ name: "Dummy" }, (user) => {
+      expect(user).toEqual(mockUser);
+    });
+  });
+});
+
+describe("remove all", () => {
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+
+  it("should remove all", async () => {
+    const mockUser = {
+      name: "Dummy",
+      email: "dummy@test.de",
+      password: "password",
+      date: "01/01/2000",
+      id: 1,
+    };
+    await User.create(mockUser, (err) => {
+      if (err) throw err;
+    });
+    regController.deleteAllRegistered();
+    User.find({}).then((result) => {
+      expect(result.length).toBe(0);
+    });
+  });
+});
