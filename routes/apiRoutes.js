13220ab465e996cc48204f77740a1ba81b814e71
Nadin <nadyawinters@gmail.com>
2021-06-10 15:41:09 +0200

    added api endpoint to show all users

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
 const Quiz = require("../models/Quiz");
+const UserModel = require("../models/UserModel");
 
-//Quizes
+//all Quizes from one User
 router.get("/library/:id", (req, res) => {
   Quiz.find({ creator: req.params.id }, (err, result) => {
     if (err) throw err;
     res.json(result);
   });
+});
+
+//show all Users
+router.get("/all", (req, res) => {
+  UserModel.find({})
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      if (error) throw error;
+    });
 });
 
 module.exports = router;
--- routes/maintainRoutes.js
+++ routes/maintainRoutes.js
@@ -1,22 +1,21 @@
-const User = require('../models/UserModel');
-const Quiz = require('../models/Quiz');
-const router = require('express').Router();
+const User = require("../models/UserModel");
+const Quiz = require("../models/Quiz");
+const router = require("express").Router();
 
-router.get("/maintain/clear", (req, res) => {
-    Quiz.remove({})
-      .then(
-        User.remove({})
-          .then(() => {
-            res.redirect("/user");
-          })
-          .catch((err) => {
-            if (err) throw err;
-          })
-      )
-      .catch((err) => {
-        if (err) throw err;
-      });
+router.get("/clear", (req, res) => {
+  Quiz.remove({})
+    .then(
+      User.remove({})
+        .then(() => {
+          res.redirect("/user");
+        })
+        .catch((err) => {
+          if (err) throw err;
+        })
+    )
+    .catch((err) => {
+      if (err) throw err;
+    });
 });
-
 
 module.exports = router;
