62c0b57da81aeef97f2bfee69db3c95ffde0fd9b
Nadin <nadyawinters@gmail.com>
2021-06-10 15:49:11 +0200

    fixed naming of ErrorController import in app.js

--- controllers/ErrorController.js
+++ controllers/ErrorController.js
@@ -1,21 +1,21 @@
 const httpStatus = require("http-status-codes");
 
-exports.respondNoResourceFound = (req, res) => {  
-    let errorCode = httpStatus.StatusCodes.NOT_FOUND;  
-    res.status(errorCode);  
-    let errormsg = '404 | Not found!';
-    res.status(404).render('error', {err: errormsg});
+exports.respondNoResourceFound = (req, res) => {
+  let errorCode = httpStatus.StatusCodes.NOT_FOUND;
+  res.status(errorCode);
+  let errormsg = "404 | Not found!";
+  res.status(404).render("error", { err: errormsg });
 };
 
-exports.respondInternalError = (error, req, res, next) => {  
-    let errorCode = httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;  
-    console.log(`ERROR occurred: ${error.stack}`);
-    res.status(errorCode);  
-    let errormsg = '500 | Internal Error!';
-    res.status(500).render('error', {err: errormsg});
+exports.respondInternalError = (error, req, res, next) => {
+  let errorCode = httpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
+  console.log(`ERROR occurred: ${error.stack}`);
+  res.status(errorCode);
+  let errormsg = "500 | Internal Error!";
+  res.status(500).render("error", { err: errormsg });
 };
 
-exports.logErrors = (error, req, res, next) => {  
-    console.error(error.stack);  
-    next(error);
+exports.logErrors = (error, req, res, next) => {
+  console.error(error.stack);
+  next(error);
 };
