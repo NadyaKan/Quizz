91f9587dfb7114bbb76b5a76848b50b630364897
Nadin <nadyawinters@gmail.com>
2021-07-01 16:52:14 +0200

    changed post url ( hardcoded for now )

--- public/vue/createQuiz.js
+++ public/vue/createQuiz.js
@@ -1,82 +1,85 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            quiz: {
-                title: '',
-                creator: userId,
-                data: [{
-                    question: '', 
-                    answers: [],
-                }],
-            },
-        };
-    },
-    methods: {
-        addBlock() {
-            this.quiz.data.push({
-                question: '',
-                answers: [],
-            });  
-        },
-        removeBlock(index) {
-            this.quiz.data.splice(index, 1);
-        },
-        async createQuiz() {
-            this.convertStringToBoolean();
-            await axios.post('http://localhost:3000/api/quiz/'+userId, this.quiz)
-            .then((res) => {
-                if(res.data.redirect == '/library'){
-                    window.location = `/quiz/library/${userId}`;
-                }
-            });
-        },
-        updateAnswers(updatedAnswers, index){
-            this.quiz.data[index].answers = updatedAnswers;
-        },
-        convertStringToBoolean() {
-            this.quiz.data.forEach(block => {
-              for(var i = 0; i < block.answers.length; i++){
-                  block.answers[i].correct = (block.answers[i].correct === 'true');
-              }
-            });
-        },
-    },
-})
-
-app.component('answer-component', {
-    emits: ['update'],
-    props: ['index'],
-    data() {
-      return {
-        answers: [
-            {
-                option: '',
-                correct: false,
-            },
+  data() {
+    return {
+      quiz: {
+        title: "",
+        creator: userId,
+        data: [
+          {
+            question: "",
+            answers: [],
+          },
         ],
-      };
+      },
+    };
+  },
+  methods: {
+    addBlock() {
+      this.quiz.data.push({
+        question: "",
+        answers: [],
+      });
     },
-    
-    methods: {
-        addAnswer() {
-            if(this.answers.length < 6){
-                this.answers.push({
-                    option: '',
-                    correct: false,
-                });
-            }
-            this.updateAnswers();
-        },
-        removeAnswer(index) {
-            this.answers.splice(index, 1);
-        },
+    removeBlock(index) {
+      this.quiz.data.splice(index, 1);
+    },
+    async createQuiz() {
+      this.convertStringToBoolean();
+      await axios
+        .post("https://wtat1-group-5.herokuapp.com/" + userId, this.quiz)
+        .then((res) => {
+          if (res.data.redirect == "/library") {
+            window.location = `/quiz/library/${userId}`;
+          }
+        });
+    },
+    updateAnswers(updatedAnswers, index) {
+      this.quiz.data[index].answers = updatedAnswers;
+    },
+    convertStringToBoolean() {
+      this.quiz.data.forEach((block) => {
+        for (var i = 0; i < block.answers.length; i++) {
+          block.answers[i].correct = block.answers[i].correct === "true";
+        }
+      });
+    },
+  },
+});
 
-        updateAnswers() {
-            this.$emit('update', this.answers);
+app.component("answer-component", {
+  emits: ["update"],
+  props: ["index"],
+  data() {
+    return {
+      answers: [
+        {
+          option: "",
+          correct: false,
         },
+      ],
+    };
+  },
+
+  methods: {
+    addAnswer() {
+      if (this.answers.length < 6) {
+        this.answers.push({
+          option: "",
+          correct: false,
+        });
+      }
+      this.updateAnswers();
+    },
+    removeAnswer(index) {
+      this.answers.splice(index, 1);
     },
 
-    template: `
+    updateAnswers() {
+      this.$emit("update", this.answers);
+    },
+  },
+
+  template: `
       <div class="answer-row" v-for="(answer, answerIndex) in answers" :key="answer._id">
       <img src="/img/remove.svg" alt="" @click="removeAnswer(answerIndex)">
       <input type="text" name="" v-model="answer.option" placeholder="Answer" @change="updateAnswers">
@@ -86,6 +89,6 @@ app.component('answer-component', {
         <button @click="addAnswer" class="centered">+</button>
       </div>
     `,
-})
+});
 
-app.mount('#app');
+app.mount("#app");
